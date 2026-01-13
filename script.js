5846cd5e15bb0b1a6824cf60885d2ac664ca2a81
Mrs-Steal-Yo-Wife <nadine.taliana.j95753@mcast.edu.mt>
2026-01-13 19:44:25 +0100

    tried to add java script
    i tried to add java script but it didn't work like ta all, so  i will fix it later

---
+++ script.js
@@ -0,0 +1,18 @@
+const buttons = document.querySelectorAll("[data-carosel-button")
+
+buttons.array.forEach( button => {
+    button.addEventListener("click", () =>{
+        const offset = button.dataset.caroselButton === "next" ? 1 : -1
+        const slides = button
+        .closest("[data-carosel]")
+        .querySelector('[data-slides')
+
+        const activeSlide = slides.querySelector("[data-active]")
+        let newIndex = [...slides.childern].indexOf(activeSlide) + offset
+        if (newIndex < 0) newIndex = slides.childern.lenght -1
+        if (newIndex >= slides.childern.lenght) newIndex = 0
+
+        slides.childern[newIndex].dataset.active = true
+        delete activeSlide.dataset.active
+    })
+});
