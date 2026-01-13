79243dbd12db77505d4a00b75f508121cda7421c
Mrs-Steal-Yo-Wife <nadine.taliana.j95753@mcast.edu.mt>
2026-01-13 20:05:23 +0100

    java still not working
    cant get the java to work o for now were leaving it and doing something else

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ buttons.array.forEach( button => {
         const slides = button
         .closest("[data-carosel]")
         .querySelector('[data-slides')
-
+        
         const activeSlide = slides.querySelector("[data-active]")
         let newIndex = [...slides.childern].indexOf(activeSlide) + offset
         if (newIndex < 0) newIndex = slides.childern.lenght -1
         if (newIndex >= slides.childern.lenght) newIndex = 0
-
+        
         slides.childern[newIndex].dataset.active = true
         delete activeSlide.dataset.active
     })
